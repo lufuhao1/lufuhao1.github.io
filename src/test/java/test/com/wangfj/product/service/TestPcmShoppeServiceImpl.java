87558ab9d8c0e80d0fcb469ddd0d9f4300fb92ae
wangxuan <1160880871@qq.com>
2016-06-06 11:04:42 +0800

    根据门店和供应商查询专柜（SAPERP导入商品时查询）

--- src/test/java/test/com/wangfj/product/service/TestPcmShoppeServiceImpl.java
+++ src/test/java/test/com/wangfj/product/service/TestPcmShoppeServiceImpl.java
@@ -3,6 +3,8 @@ package test.com.wangfj.product.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.wangfj.product.organization.domain.vo.PcmShoppeResultDto;
+import com.wangfj.product.organization.domain.vo.SelectPcmShoppeDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,39 +14,52 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.wangfj.product.organization.service.intf.IPcmShoppeService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class TestPcmShoppeServiceImpl {
 
-	@Autowired
-	private IPcmShoppeService shoppeService;
+    @Autowired
+    private IPcmShoppeService shoppeService;
 
-	@Test
-	public void generateShoppeCode() {
+    @Test
+    public void findShoppeForSAPERPImport() {
+        SelectPcmShoppeDto dto = new SelectPcmShoppeDto();
+//        dto.setShopCode("D001");
+//        dto.setSupplyCode("0007000091");
+//        dto.setSupplyCode("0004500000");
 
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("sid", "149");
-		paramMap.put("shopCode", "40001");
-		String code = shoppeService.generateShoppeCode(paramMap);
-		System.out.println(code);
-	}
+        dto.setShopCode("21011");
+        dto.setSupplyCode("0008660");
+        PcmShoppeResultDto shoppe = shoppeService.findShoppeForSAPERPImport(dto);
+        System.out.println(shoppe);
+    }
 
-	@Test
-	public void generateEShoppeCode() {
+    @Test
+    public void generateShoppeCode() {
 
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("shopCode", "D001");
-		paramMap.put("shoppeCodeStart", "2");
-		String code = shoppeService.generateEShoppeCode(paramMap);
-		System.out.println(code);
-	}
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("sid", "149");
+        paramMap.put("shopCode", "40001");
+        String code = shoppeService.generateShoppeCode(paramMap);
+        System.out.println(code);
+    }
 
-	@Test
-	public void generateEBusinessShoppeCode() {
+    @Test
+    public void generateEShoppeCode() {
 
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("shopCode", "D001");
-		String code = shoppeService.generateEBusinessShoppeCode(paramMap);
-		System.out.println(code);
-	}
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("shopCode", "D001");
+        paramMap.put("shoppeCodeStart", "2");
+        String code = shoppeService.generateEShoppeCode(paramMap);
+        System.out.println(code);
+    }
+
+    @Test
+    public void generateEBusinessShoppeCode() {
+
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("shopCode", "D001");
+        String code = shoppeService.generateEBusinessShoppeCode(paramMap);
+        System.out.println(code);
+    }
 
 }
