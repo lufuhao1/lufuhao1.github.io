40b330ae20ed0baba8f72b19bb8c7c06b1c3db51
wangxuan <1160880871@qq.com>
2016-06-23 13:39:34 +0800

    Test

--- src/test/java/test/com/wangfj/product/service/TestPcmBrandServiceImpl.java
+++ src/test/java/test/com/wangfj/product/service/TestPcmBrandServiceImpl.java
@@ -3,6 +3,7 @@ package test.com.wangfj.product.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
 import com.wfj.platform.util.zookeeper.discovery.SpringMvcServiceProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,12 +27,90 @@ public class TestPcmBrandServiceImpl {
     @Test
     public void test() {
 
-        try {
-            String serviceAddress = provider.provideServiceAddress("pcm-item-query");
-            System.out.println(serviceAddress);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Gson gson = new Gson();
+        String json = "{\n" +
+                "  \"data\": {\n" +
+                "    \"currentPage\": 1,\n" +
+                "    \"pageSize\": 10,\n" +
+                "    \"count\": 1,\n" +
+                "    \"pages\": 1,\n" +
+                "    \"start\": 0,\n" +
+                "    \"limit\": 10,\n" +
+                "    \"list\": [\n" +
+                "      {\n" +
+                "        \"sid\": 15,\n" +
+                "        \"supplyName\": \"20160311测试供应商代销*D\",\n" +
+                "        \"postcode\": null,\n" +
+                "        \"city\": null,\n" +
+                "        \"country\": null,\n" +
+                "        \"zone\": null,\n" +
+                "        \"shopRegion\": null,\n" +
+                "        \"address\": null,\n" +
+                "        \"phone\": null,\n" +
+                "        \"fax\": null,\n" +
+                "        \"email\": null,\n" +
+                "        \"lastOptUser\": null,\n" +
+                "        \"lastOptDates\": null,\n" +
+                "        \"lastOptDateStr\": null,\n" +
+                "        \"shopSid\": \"21011\",\n" +
+                "        \"supplyCode\": \"000868K\",\n" +
+                "        \"supplyType\": null,\n" +
+                "        \"status\": null,\n" +
+                "        \"shortName\": null,\n" +
+                "        \"businessPattern\": \"1\",\n" +
+                "        \"street\": null,\n" +
+                "        \"orgCode\": null,\n" +
+                "        \"industry\": null,\n" +
+                "        \"bizCertificateNo\": null,\n" +
+                "        \"taxType\": null,\n" +
+                "        \"taxNumbe\": null,\n" +
+                "        \"bank\": null,\n" +
+                "        \"bankNo\": null,\n" +
+                "        \"registeredCapital\": null,\n" +
+                "        \"enterpriseProperty\": null,\n" +
+                "        \"businessCategory\": null,\n" +
+                "        \"legalPerson\": null,\n" +
+                "        \"legalPersonIcCode\": null,\n" +
+                "        \"legalPersonContact\": null,\n" +
+                "        \"agent\": null,\n" +
+                "        \"agentIcCode\": null,\n" +
+                "        \"agentContact\": null,\n" +
+                "        \"contact\": null,\n" +
+                "        \"contactTitle\": null,\n" +
+                "        \"contactIcCode\": null,\n" +
+                "        \"contactWay\": null,\n" +
+                "        \"businessScope\": null,\n" +
+                "        \"keySupplier\": null,\n" +
+                "        \"taxRates\": 17,\n" +
+                "        \"taxRateStr\": \"17.000000\",\n" +
+                "        \"inOutCity\": null,\n" +
+                "        \"admissionDate\": null,\n" +
+                "        \"returnSupply\": null,\n" +
+                "        \"joinSite\": null,\n" +
+                "        \"apartOrder\": null,\n" +
+                "        \"dropship\": null,\n" +
+                "        \"erpSupplierCode\": null,\n" +
+                "        \"zlyFlag\": null,\n" +
+                "        \"zzxxhcFlag\": null,\n" +
+                "        \"field1\": null,\n" +
+                "        \"field2\": null,\n" +
+                "        \"field3\": null,\n" +
+                "        \"actionCode\": null,\n" +
+                "        \"zflg\": null\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"success\": \"true\"\n" +
+                "}";
+        String toJson = gson.toJson(json);
+        System.out.println(toJson);
+
+//        try {
+//            String serviceAddress = provider.provideServiceAddress("pcm-item-query");
+//            System.out.println(serviceAddress);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 
