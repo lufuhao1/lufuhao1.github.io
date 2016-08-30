d67416b3b4e36be4ec454656ce8c9de2d75b4d30
wangxuan <1160880871@qq.com>
2016-08-30 10:36:53 +0800

    门店信息添加，小bug修复，修改sid问题。

--- src/test/java/test/com/wangfj/product/controller/TestPcmOrganizationMainController.java
+++ src/test/java/test/com/wangfj/product/controller/TestPcmOrganizationMainController.java
@@ -9,122 +9,125 @@ import com.wangfj.product.core.controller.support.SelectPcmOrganizationPara;
 
 /**
  * 组织机构测试
- * 
+ *
  * @Class Name TestPcmOrganizationMainController
  * @Author wangx
  * @Create In 2015-8-18
  */
 public class TestPcmOrganizationMainController {
 
-	@Test
-	public void saveOrUpdateOrg() {
+    @Test
+    public void saveOrUpdateOrg() {
 
-		PcmOrgPara para = new PcmOrgPara();
+        PcmOrgPara para = new PcmOrgPara();
 
-		para.setOrganizationType(0);
-		// para.setOrganizationType(3);
-		para.setGroupSid(0L);
-		para.setOrganizationCode("1511031341");
-		para.setOrganizationName("北京王府井集团11");
-		para.setOrganizationStatus(0);
-		para.setParentSid("0");
-		// para.setStoreType(0);
-		// para.setStoreType(2);
-		para.setActionCode("A");
+//		para.setOrganizationType(0);
+        para.setOrganizationType(3);
+        para.setGroupSid(1L);
+        para.setOrganizationCode("21011");
+        para.setOrganizationName("百货大楼");
+//		para.setOrganizationStatus(0);
+        para.setParentSid("12");
+        // para.setStoreType(0);
+        // para.setStoreType(2);
+        para.setActionCode("U");
 
-		String jsonString = JsonUtil.getJSONString(para);
-		System.out.println(jsonString);
-		String response = HttpUtil.doPost(
-				"http://127.0.0.1:8043/pcm-admin-sdc/organization/saveOrUpdateOrg.htm",
-				JsonUtil.getJSONString(para));
-		System.out.println(response);
-	}
+//        para.setPostCode("2109312");
+        para.setBank("中国建设银行");
 
-	/**
-	 * 查询所有组织机构的部分信息
-	 * 
-	 * @Methods Name findListOrgPart
-	 * @Create In 2015-8-20 By wangxuan void
-	 */
-	@Test
-	public void findListOrgPart() {
+        String jsonString = JsonUtil.getJSONString(para);
+        System.out.println(jsonString);
+        String response = HttpUtil.doPost(
+                "http://127.0.0.1:8043/pcm-admin-sdc/organization/saveOrUpdateOrg.htm",
+                JsonUtil.getJSONString(para));
+        System.out.println(response);
+    }
 
-		SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
+    /**
+     * 查询所有组织机构的部分信息
+     *
+     * @Methods Name findListOrgPart
+     * @Create In 2015-8-20 By wangxuan void
+     */
+    @Test
+    public void findListOrgPart() {
 
-		// para.setOrganizationType("0");
-		para.setOrganizationType(3);
-		// para.setCurrentPage(1);
-		// para.setCurrentPage("2");
-		// para.setOrganizationCode("21011");
-		// para.setOrganizationName("北京");
-		// para.setOrganizationStatus(0);
-		// para.setPageSize(10);
-		// para.setParentSid("");
-		// para.setStoreType(0);
-		// para.setStoreType("2");
+        SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
 
-		String response = HttpUtil.doPost(
-				"http://127.0.0.1:8043/pcm-admin-sdc/organization/findListOrgPart.htm",
-				JsonUtil.getJSONString(para));
-		System.out.println(response);
-	}
+        // para.setOrganizationType("0");
+        para.setOrganizationType(3);
+        // para.setCurrentPage(1);
+        // para.setCurrentPage("2");
+        // para.setOrganizationCode("21011");
+        // para.setOrganizationName("北京");
+        // para.setOrganizationStatus(0);
+        // para.setPageSize(10);
+        // para.setParentSid("");
+        // para.setStoreType(0);
+        // para.setStoreType("2");
 
-	/**
-	 * 查询所有组织机构信息
-	 * 
-	 * @Methods Name findListOrganization
-	 * @Create In 2015-8-20 By wangxuan void
-	 */
-	@Test
-	public void findListOrganization() {
+        String response = HttpUtil.doPost(
+                "http://127.0.0.1:8043/pcm-admin-sdc/organization/findListOrgPart.htm",
+                JsonUtil.getJSONString(para));
+        System.out.println(response);
+    }
 
-		SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
+    /**
+     * 查询所有组织机构信息
+     *
+     * @Methods Name findListOrganization
+     * @Create In 2015-8-20 By wangxuan void
+     */
+    @Test
+    public void findListOrganization() {
 
-		// para.setOrganizationType("0");
-		para.setOrganizationType(3);
-		// para.setCurrentPage(1);
-		// para.setCurrentPage("2");
-		// para.setOrganizationCode("21011");
-		// para.setOrganizationName("北京");
-		// para.setOrganizationStatus(0);
-		// para.setPageSize(10);
-		// para.setParentSid("");
-		// para.setStoreType(0);
-		// para.setStoreType("2");
+        SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
 
-		String response = HttpUtil.doPost(
-				"http://127.0.0.1:8043/pcm-admin-sdc/organization/findListOrganization.htm",
-				JsonUtil.getJSONString(para));
-		System.out.println(response);
-	}
+        // para.setOrganizationType("0");
+        para.setOrganizationType(3);
+        // para.setCurrentPage(1);
+        // para.setCurrentPage("2");
+        // para.setOrganizationCode("21011");
+        // para.setOrganizationName("北京");
+        // para.setOrganizationStatus(0);
+        // para.setPageSize(10);
+        // para.setParentSid("");
+        // para.setStoreType(0);
+        // para.setStoreType("2");
 
-	/**
-	 * 分页查找测试
-	 * 
-	 * @Methods Name findPageOrganization
-	 * @Create In 2015-8-18 By wangx void
-	 */
-	@Test
-	public void findPageOrganization() {
+        String response = HttpUtil.doPost(
+                "http://127.0.0.1:8043/pcm-admin-sdc/organization/findListOrganization.htm",
+                JsonUtil.getJSONString(para));
+        System.out.println(response);
+    }
 
-		SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
+    /**
+     * 分页查找测试
+     *
+     * @Methods Name findPageOrganization
+     * @Create In 2015-8-18 By wangx void
+     */
+    @Test
+    public void findPageOrganization() {
 
-		// para.setOrganizationType("0");
-		para.setOrganizationType(3);
-		para.setCurrentPage(1);
-		// para.setCurrentPage("2");
-		// para.setOrganizationCode("21011");
-		para.setOrganizationName("北京");
-		para.setOrganizationStatus(0);
-		para.setPageSize(10);
-		para.setParentSid("");
-		para.setStoreType(0);
-		// para.setStoreType("2");
+        SelectPcmOrganizationPara para = new SelectPcmOrganizationPara();
 
-		String response = HttpUtil.doPost(
-				"http://127.0.0.1:8081/pcm-admin-sdc/organization/findPageOrganization.htm",
-				JsonUtil.getJSONString(para));
-		System.out.println(response);
-	}
+        // para.setOrganizationType("0");
+        para.setOrganizationType(3);
+        para.setCurrentPage(1);
+        // para.setCurrentPage("2");
+        // para.setOrganizationCode("21011");
+        para.setOrganizationName("北京");
+        para.setOrganizationStatus(0);
+        para.setPageSize(10);
+        para.setParentSid("");
+        para.setStoreType(0);
+        // para.setStoreType("2");
+
+        String response = HttpUtil.doPost(
+                "http://127.0.0.1:8081/pcm-admin-sdc/organization/findPageOrganization.htm",
+                JsonUtil.getJSONString(para));
+        System.out.println(response);
+    }
 
 }
