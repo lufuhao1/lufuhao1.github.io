e7b07021595597bd6f99e2a105fd834183c412cd
lfh123 <18814729683@163.com>
2017-03-22 14:19:00 +0800

    test gitcfz@115.28.75.68:repositories/mytest.git

---
+++ TestGit/src/com/Test.java
@@ -0,0 +1,15 @@
+package com;
+/****************
+ *Author:卢福浩
+ *Day:2017年3月22日
+ *Time:下午2:15:09
+ *Functions: 
+ *****************/
+public class Test {
+	public static void mian(String[] args){
+		for(int i=0;i<10;i++){
+			
+		}
+	}
+
+}
